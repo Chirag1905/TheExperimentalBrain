441ad84f1259417e1734a17234a6463fb553c92b
dinesh.suthar@ifair.com <dinesh.suthar@iflair.com>
2025-02-11 16:45:03 +0530

    path update

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Suspense fallback={<Loadding />}>
     <ToastContainer />
-      <BrowserRouter>
+      <BrowserRouter basename="/teb/">
         <Routes>
           <Route path="/" element={<ChatLogin />} />
           <Route path="/authenticate" element={<AppAuthenticate />} />
